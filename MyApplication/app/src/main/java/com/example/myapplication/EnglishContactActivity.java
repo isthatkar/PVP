31f06c0dbf77b53e9c0d7a3239202f220aa4725b
Rugile Karengaite <rugile.karengaite@ktu.edu>
2022-05-28 22:56:15 +0300

    added sources pages

--- MyApplication/app/src/main/java/com/example/myapplication/EnglishContactActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/EnglishContactActivity.java
@@ -2,15 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EnglishContactActivity extends AppCompatActivity {
+    private Button sourcesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.english_activity_contact);
 
         getSupportActionBar().hide();
+
+        sourcesButton     = (Button) findViewById(R.id.button_saltiniai);
+        sourcesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSources();
+            }
+        });
+
+    }
+    public void openSources(){
+        Intent intent = new Intent(this, EnglishSourcesActivity.class);
+        startActivity(intent);
     }
 }
--- MyApplication/app/src/main/java/com/example/myapplication/LithuanianContactActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/LithuanianContactActivity.java
@@ -2,15 +2,33 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LithuanianContactActivity extends AppCompatActivity {
+    private Button sourcesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
         getSupportActionBar().hide();
+
+        sourcesButton     = (Button) findViewById(R.id.button_saltiniai);
+        sourcesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSources();
+            }
+        });
+
+    }
+    public void openSources(){
+        Intent intent = new Intent(this, LithuanianSourcesActivity.class);
+        startActivity(intent);
     }
 }
